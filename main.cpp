f0abe6391690b56f73a2115c102eed7b2c0f6cb8
hatone <b1006006@gmail.com>
2011-03-15 15:45:53 +0900

    質量重心算出関数の動作確認済み

--- main.cpp
+++ main.cpp
@@ -12,21 +12,24 @@ void pct(GrCoord mrkr, Cluster c)
 {
 //ここに処理書く
     std::cout<<"PCT"<<std::endl;
-    
+    Vec3d wFact;
     // 適当なグローバル座標群を作成
     std::vector<Vec3d> coordinates;
+    std::vector<double> mass;
     int size = 10;
     coordinates.reserve(size);
     for (int i = 0; i < size; i++)
     {
         Vec3d v(i, i+1, i+2);
         coordinates.push_back(v);
+        mass.push_back(1);
         
-        std::cout<< v.x << std::endl;
     }
     
     // グローバル座標群を代入
     mrkr.setCoord(coordinates);
+    wFact=mrkr.weightFactor(coordinates,mass);
+
 }
 void display (Cluster f, Cluster t)
 {
@@ -45,8 +48,6 @@ int main(int argc, char *argv[])
 	pct(mrkrT, t);
     display(f, t);
 	
-	std::cout << "yeah!!!!!!!!!!" << std::endl;
-	std::cout << "nuha-!!!!!!!!!" << std::endl;
 	
     return 0;
 
--- pct.hpp
+++ pct.hpp
@@ -7,43 +7,43 @@
 #ifndef _PCT_HPP_
 #define _PCT_HPP_
 
-class Vec4d
-{
-public:
-    double x; //!< Vector of x
-    double y; //!< Vector of y
-    double z; //!< Vector of z
-    double m; //!< Vector of m
-    
-    inline Vec4d(): x(0.0), y(0.0), z(0.0), m(0.0) {}
-    inline Vec4d(double _x, double _y, double _z, double _m)
-    {
-        x = _x;
-        y = _y;
-        z = _z;
-        m = _m;
-    }
-    
-    /*!
-     *  \brief 積
-     *  \param n 掛ける数
-     */
-    inline Vec4d prod(double n)
-    {
-        return Vec4d(x*n, y*n, z*n, m);
-    }
-    
-    inline Vec4d sum(Vec4d v)
-    {
-        return Vec4d(x+v.x, y+v.y, z+v.z, m);
-    }
-
-    
-};
+//class Vec4d
+//{
+//public:
+//    double x; //!< Vector of x
+//    double y; //!< Vector of y
+//    double z; //!< Vector of z
+//    double m; //!< Vector of m
+//    
+//    inline Vec4d(): x(0.0), y(0.0), z(0.0), m(0.0) {}
+//    inline Vec4d(double _x, double _y, double _z, double _m)
+//    {
+//        x = _x;
+//        y = _y;
+//        z = _z;
+//        m = _m;
+//    }
+//    
+//    /*!
+//     *  \brief 積
+//     *  \param n 掛ける数
+//     */
+//    inline Vec4d prod(double n)
+//    {
+//        return Vec4d(x*n, y*n, z*n, m);
+//    }
+//    
+//    inline Vec4d sum(Vec4d v)
+//    {
+//        return Vec4d(x+v.x, y+v.y, z+v.z, m);
+//    }
+//
+//    
+//};
 
 /*!
  *  \class LoCoord
- *  \brief nya-
+ *  \brief This is Local coordinate class
  *
  *
  */
@@ -54,7 +54,7 @@ public:
 
 /*!
  *  \class GrCoord
- *  \brief bow
+ *  \brief This is grobal coordinate class
  *
  *
  */
@@ -64,12 +64,20 @@ public:
     std::vector<Vec3d> m_coordinates;
     std::vector<double> m_mass;
     
+    /*!
+     *  \brief 任意の値をセットする？
+     *  \param coordinate 代入したいグローバル座標群
+     */
     inline void setCoord(std::vector<Vec3d> &coordinates)
     {
         m_coordinates = coordinates;
     }
     
-    inline Vec3d weightFactor(std::vector<Vec3d> coordinates)
+    /*!
+     *  \brief 質量重心
+     *  \param coordinatesグローバル座標群, mass質量群
+     */
+    inline Vec3d weightFactor(std::vector<Vec3d> coordinates, std::vector<double> mass)
     {
         
         Vec3d c,gm;
@@ -77,10 +85,9 @@ public:
         
         for(int i=0; i< coordinates.size(); i++)
         {
-            m += m_mass[i];
-            gm.sum(coordinates[i].prod(m_mass[i]));
+            m += mass[i];
+            gm=gm.sum(coordinates[i]);
         }
-        
         c = gm.divis(m);
         return c;
     }
